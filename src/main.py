4aade4b8315b549d2a9cb245d0495ee1b5d9d55c
Vincent Atmadja <vincentpatmadja@gmail.com>
2022-02-28 17:41:30 +0700

    Clean library and make main program

--- src/main.py
+++ src/main.py
@@ -1,34 +1,135 @@
-import numpy as np
+from matplotlib.transforms import Bbox
+from sklearn import datasets
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.spatial import ConvexHull
-import myConvexHull
+from myConvexHull import convexhull
 
-from sklearn import datasets
-data = datasets.load_iris()
+color = {
+    "blue" : "b",
+    "green" : "g",
+    "red" : "r",
+    "cyan" : "c",
+    "magenta" : "m",
+    "yellow" : "y",
+    "black" : "k",
+    "white" : "w"
+}
 
-#create a DataFrame
-df = pd.DataFrame(data.data, columns=data.feature_names)
-print(data.feature_names)
+color_list = ["blue", "green", "red", "cyan", "magenta", "yellow", "black", "white"]
 
-df['Target'] = pd.DataFrame(data.target)  
-plt.figure(figsize = (10, 6)) 
-colors = ['b','r','g'] 
-plt.title('Sepal Width vs Sepal Length') 
-plt.xlabel(data.feature_names[0]) 
-plt.ylabel(data.feature_names[1]) 
-for i in range(len(data.target_names)): 
-    bucket = df[df['Target'] == i] 
-    bucket = bucket.iloc[:,[0,1]].values
+def getDatabase():
+    print("Welcome to ConvexHull")
+    print("Currenlt, we only works for 3 databases: ")
+    print("1. Iris")
+    print("2. Wine")
+    print("3. Breast_Cancer")
 
-    hull = myConvexHull.convexhull(bucket)
-    # hull = ConvexHull(bucket) #bagian ini diganti dengan hasil implementasi ConvexHull Divide & Conquer 
-    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i]) 
-    for simplex in hull: 
-        # print(simplex, bucket[simplex[0]], bucket[simplex[1]])
-        plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i])
-    # print("\n") 
+    choice = 0
+    while(True):
+        choice = int(input("Which databases do you want to use (input the number)? "))
 
-plt.legend()
+        if(choice >= 1 and choice <= 3):
+            break
+        else:
+            print("That number is not available, please input another number")
+    
+    if(choice == 1):
+        return datasets.load_iris()
+    elif(choice == 2):
+        return datasets.load_wine()
+    else:
+        return datasets.load_breast_cancer()
+    
+def getFeatureName(data):
+    i = 1
+    featureNames = {}
 
-plt.savefig("../test/output/sepalv3.jpg", bbox_inches='tight')
+    for each in data.feature_names:
+        featureNames[i] = each
+        i += 1
+
+    print("Available feature to be plotted: ")
+    
+    for each in featureNames:
+        print("{}.{}".format(each, featureNames[each]))
+    
+    id1 = 0
+    id2 = 0
+
+    while(True):
+        id1 = int(input("Which features do you want to use for x axis (input the number)? "))
+        
+        if(id1 >= 1 and id1 <= len(featureNames)):
+            break
+        else:
+            print("That number is not available, please input another number")
+
+    while(True):
+        id2 = int(input("Which features do you want to use for y axis (input the number)? "))
+        
+        if(id2 >= 1 and id2 <= len(featureNames) and id2 != id1 ):
+            break
+        elif(id1 == id2):
+            print("That number is already selected for feature in x axis, please input another number")
+        else:
+            print("That number is not available, please input another number")
+    
+    return featureNames, id1 - 1, id2 - 1
+
+def getColor(count):
+    selectedColor = []
+    print("Now choose, {} colors for your line".format(count))
+    print("Available color: ")
+
+    for each in color_list:
+        print("  {}".format(each))
+    
+    while(count > 0):
+        select = ""
+        while(True):
+            select = input("Which color do you want to use? ")
+
+            if(select.lower() in color_list):
+                selectedColor.append(color[select])
+                break
+            else:
+                print("Those color is not available, please input another color")
+
+        
+        count -= 1
+        
+        if(count != 0):
+            print("Next Color !")
+
+    return selectedColor
+
+def main():
+    data = getDatabase()
+    df = pd.DataFrame(data.data, columns=data.feature_names)
+
+    feature_names, id1, id2 = getFeatureName(data)
+    df['Target'] = pd.DataFrame(data.target)
+
+    plt.figure(figsize = (10, 6)) 
+    plt.title('{} vs {}'.format(feature_names[id1 + 1], feature_names[id2 + 1]))
+    plt.xlabel(data.feature_names[id1]) 
+    plt.ylabel(data.feature_names[id2]) 
+
+    selected_color = getColor(len(data.target_names))
+
+    for i in range(len(data.target_names)): 
+        bucket = df[df['Target'] == i] 
+        bucket = bucket.iloc[:,[id1, id2]].values
+
+        hull = convexhull(bucket)
+        plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i], color=selected_color[i]) 
+        for simplex in hull: 
+            plt.plot(bucket[simplex, 0], bucket[simplex, 1], selected_color[i])
+
+    plt.legend()
+
+    output = input("Please choose name for your output file: ")
+    plt.savefig("../test/output/{}.jpg".format(output), bbox_inches="tight")
+
+if (__name__ == "__main__"):
+    main()
--- src/myConvexHull.py
+++ src/myConvexHull.py
@@ -1,10 +1,9 @@
-from cmath import sqrt
 from copy import deepcopy
-from numpy import arange, arccos
 from math import isclose, acos
 
 def determinant(p1, p2, p3):
-    #Pengecekan posisi p3 dibanding dengan p1 p2
+    #menghitung determinan dari p1, p2, p3 -> p1 dan p2 membentuk garis dan 
+    #p1, p2, dan p3 adalah pasangan titik
     x1, y1 = p1[0], p1[1]
     x2, y2 = p2[0], p2[1]
     x3, y3 = p3[0], p3[1]
@@ -12,65 +11,72 @@ def determinant(p1, p2, p3):
     return (x1*y2) + (x3 * y1) + (x2 * y3) - (x3 * y2) - (x2 * y1) - (x1 * y3)
 
 def findLength(p1, p2):
+    #Menghitung panjang garis yang dibentuk oleh titik p1 dan p2 
     difx = p1[0] - p2[0]
     dify = p1[1] - p2[1]
 
     return ((difx ** 2) + (dify ** 2)) ** 0.5
 
 def findCosine(find, param1, param2):
+    #Menghitung cosine yang dibentuk oleh garis find param1, dan param2
+    #Sudut yang dibentuk adalah sudut antara param1 dan param2, dengan find adalah sisi di depan sudut
+    #Masing-masing find, param1, dan param2 berisi panjang dari masing" garis
+
     denom = (param1 ** 2) + (param2 ** 2) - (find ** 2)
     denom /= (2 * param2 * param1)
     return denom
 
-def findDistance(cosine, param1):
-    param2 = cosine * param1
-    return ((param1 ** 2) - (param2 ** 2)) ** 0.5
-
 def checkexist(array, toCheck):
+    #Mengecek kemuncukan titik toCheck di arrays
     for each in array:
         if(each[0] == toCheck[0] and each[1] == toCheck[1]):
             return True
-        elif(each[0] == toCheck[1] and each[1] == toCheck[0]):
-            return True
     return False
 
-def creteDict(data):
+def createDict(data):
+    #Membuat dictionary dengan key berupa pasangan point dan value adalah indexnya di dataset awal
     dataInDict = {}
+    
     for i in range(len(data)):
         dataInDict[tuple(data[i])] = i 
+
     return dataInDict
 
 
+def preprocessing(data):
+    dataInDict = createDict(data)
+
+    cleanData = []
+    [cleanData.append(x) for x in data if not checkexist(cleanData, x)]
+
+    sortedData = sorted(cleanData, key = lambda x: (x[0], x[1]))
+
+    return dataInDict, sortedData
+
+
 def convexhull(data):
-    copy_Data = deepcopy(data)
-    dataInDict = creteDict(copy_Data)
+    #Membuat deepcopy dari data awal untuk mengurangi risiko kerusakan data awal
+    copyData = deepcopy(data)
     
-    clean_Data = []
-    [clean_Data.append(x) for x in copy_Data if not checkexist(clean_Data, x)]
-
-    sorted_data = sorted(clean_Data, key = lambda x: (x[0], x[1]))
-    length = len(sorted_data)
-
-    p1 = sorted_data[0]
-    p2 = sorted_data[length - 1]
+    dataInDict, sortedData = preprocessing(copyData)
+    
+    p1 = sortedData[0]
+    p2 = sortedData[len(sortedData) - 1]
 
     left_part = []
     right_part = []
 
-    for i in range(1, length - 1):
-        if(determinant(p1, p2, sorted_data[i]) > 0):
-            left_part.append(sorted_data[i])
-        elif(determinant(p1, p2, sorted_data[i]) < 0):
-            right_part.append(sorted_data[i])
+    for i in range(1, len(sortedData) - 1):
+        if(determinant(p1, p2, sortedData[i]) > 0):
+            left_part.append(sortedData[i])
+        elif(determinant(p1, p2, sortedData[i]) < 0):
+            right_part.append(sortedData[i])
     
-    final = []
+    hullPair = []
+    DCStep(left_part, p1, p2, hullPair, dataInDict)
+    DCStep(right_part, p2, p1, hullPair, dataInDict)
 
-    print("Left: \n")
-    DCStep(left_part, p1, p2, final, dataInDict)
-    print("Right: \n")
-    DCStep(right_part, p2, p1, final, dataInDict)
-
-    return final
+    return hullPair
 
 def DCStep(array, p1, p2, final, dataInDict):
     if(len(array) == 0):
@@ -79,14 +85,11 @@ def DCStep(array, p1, p2, final, dataInDict):
         newPair.append(dataInDict[tuple(p2)])
 
         if(not checkexist(final, newPair)):
-            print(p1, p2)
             final.append(newPair)
     else:
-        for each in array:
-            print(each)
-        print("\n")
         length = findLength(p1, p2)
         #length -> p1 p2
+
         maxLength = 0
         maxAngle = 0.0
         p3 = []
@@ -96,8 +99,13 @@ def DCStep(array, p1, p2, final, dataInDict):
             length3 = findLength(p2, array[i])
 
             cosine = findCosine(length3, length2, length)
+            
+            #!Ini kasus cosine > 1, dalam beberapa uji coba terjadi karena ketidakdepatan float, saya belum menemukan solusinya
+            if(cosine > 1):
+                cosine = 1
             area = determinant(p1, p2, array[i])
             distance = abs(area) / length
+            
             if(distance > maxLength):
                 maxLength = distance
                 p3 = array[i]
@@ -115,21 +123,11 @@ def DCStep(array, p1, p2, final, dataInDict):
         if(len(p3) != 0):
             for i in range(len(array)):
                 if(array[i][0] != p3[0] or array[i][1] != p3[1]):
-                    print(array[i], determinant(p1, p3, array[i]))
-                    print(array[i], determinant(p2, p3, array[i]))
-                    print("")
                     #Sebelah kiri garis p1pmax
                     if(determinant(p1, p3, array[i]) > 0):
                         first_part.append(array[i])    
                     #Sebelah kanan garis p2pmax
                     if(determinant(p2, p3, array[i]) < 0):
                         second_part.append(array[i])
-
-            print(p1, p2, p3)
-            print("\n")
-            print("First: \n")
-            print(first_part)
-            print("Second: \n")
-            print(second_part)
             DCStep(first_part, p1, p3, final, dataInDict)
             DCStep(second_part, p3, p2, final, dataInDict)
